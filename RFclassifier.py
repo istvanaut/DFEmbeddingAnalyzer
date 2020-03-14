c2fddefe3cb583e869aa78723953a9272c7a32e9
Istvan F <ifjistvanfabian@gmail.com>
2020-03-14 13:42:08 +0100

    Implemented GridSearchCV

--- RFclassifier.py
+++ RFclassifier.py
@@ -18,6 +18,8 @@ from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
+from sklearn.utils import shuffle
+
 
 def listAverage(lst):
     return sum(lst) / len(lst)
@@ -184,69 +186,75 @@ for i in range(featureVectLength):
 
 featureVectColumns.append("target")
 
-df = pd.DataFrame(data, columns=featureVectColumns) #creates a new dataframe that's empty
+df = pd.DataFrame(data, columns=featureVectColumns)
+df = df.sample(frac=1).reset_index(drop=True)
 
 dfData = df.drop([featureVectColumns[-1], featureVectColumns[-2]], axis="columns")
 dfTarget = df["target"]
 
-model_params = {
-    'svm': {
-        'model': SVC(gamma='auto'),
-        'params' : {
-            'C': [1,10,20,50,100],
-            'kernel': ['rbf','linear']
-        }
-    },
-    'random_forest': {
-        'model': RandomForestClassifier(),
-        'params' : {
-            'n_estimators': [10, 50,100,300]
-        }
-    },
-    'logistic_regression' : {
-        'model': LogisticRegression(solver='liblinear',multi_class='auto'),
-        'params': {
-            'C': [1,5,10,50, 100]
-        }
-    }
-}
-
-scores = []
-
-for model_name, mp in model_params.items():
-    clf = GridSearchCV(mp['model'], mp['params'], cv=3, return_train_score=True)
-    clf.fit(dfData, dfTarget)
-    scores.append({
-        'model': model_name,
-        'best_score': clf.best_score_,
-        'best_params': clf.best_params_
-    })
-
-dfResults = pd.DataFrame(scores, columns=['model', 'best_score', 'best_params'])
-
-print(dfResults)
+# model_params = {
+#     'svm': {
+#         'model': SVC(gamma='auto'),
+#         'params' : {
+#             'C': [1,10,20,50,100],
+#             'kernel': ['rbf','linear']
+#         }
+#     },
+#     'random_forest': {
+#         'model': RandomForestClassifier(),
+#         'params' : {
+#             'n_estimators': [10, 50,100,300]
+#         }
+#     },
+#     'logistic_regression' : {
+#         'model': LogisticRegression(solver='liblinear',multi_class='auto'),
+#         'params': {
+#             'C': [1,5,10,50, 100]
+#         }
+#     }
+# }
+#
+# scores = []
+#
+# for model_name, mp in model_params.items():
+#     clf = GridSearchCV(mp['model'], mp['params'], cv=3, return_train_score=True)
+#     clf.fit(dfData, dfTarget)
+#     scores.append({
+#         'model': model_name,
+#         'best_score': clf.best_score_,
+#         'best_params': clf.best_params_
+#     })
+#
+# dfResults = pd.DataFrame(scores, columns=['model', 'best_score', 'best_params'])
+#
+# print(dfResults)
 
 # print(listAverage(cross_val_score(LogisticRegression(), dfData, dfTarget, cv=3)))
 # print(listAverage(cross_val_score(SVC(), dfData, dfTarget, cv=3)))
 # print(listAverage(cross_val_score(RandomForestClassifier(), dfData, dfTarget, cv=3)))
 
+clf = GridSearchCV(RandomForestClassifier(), {'n_estimators': [5, 10, 20, 30, 50, 100, 300]}, cv=5, return_train_score=False)
+clf.fit(dfData, dfTarget)
 
-# X_train, X_test, y_train, y_test = train_test_split(df.drop([featureVectColumns[-1], featureVectColumns[-2]],
-#                                                             axis="columns"), df["target"], test_size=0.2)
+clfDf = pd.DataFrame(clf.cv_results_)
+
+print(clfDf[['param_n_estimators', 'split0_test_score', 'split1_test_score', 'split2_test_score', 'mean_test_score']])
+
+# X_train, X_test, y_train, y_test = train_test_split(dfData, dfTarget, test_size=0.2)
 
 # print("X_train length ", len(X_train))
 # print("X_test length ", len(X_test))
 # print("y_train length ", len(y_train))
 # print("y_test length ", len(y_test))
 #
-# model = RandomForestClassifier(n_estimators=500)
+# model = RandomForestClassifier()
 # model.fit(X_train, y_train)
-
+#
 # pickle.dump(model, open("RFmodel.pickle", 'wb'))
-
+#
 # score = model.score(X_test, y_test)
 # print("test score = ", score)
-
+#
 # y_predicted = model.predict(X_test)
 #
 # cm = confusion_matrix(y_test, y_predicted)
